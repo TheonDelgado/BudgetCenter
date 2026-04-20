ce87e76d32fa938bc8b6d1fdd53b28c41156aa4c
TheonDelgado <delgado_theon@yahoo.com>
2026-04-19 23:44:17 -0400

    Home

--- BudgetCenter-fe/src/app/page.js
+++ BudgetCenter-fe/src/app/page.js
@@ -1,9 +1,24 @@
+
 import "./Home.css"
 
 export default function Home() {
   return (
-    <div className="home">
-      <p className="texter">Home page</p>
+    <div className="dashboard">
+
+      <h4 className="welcome-text">Welcome User</h4>
+
+      <div className="top-header">
+        <h1 className="main-header">Financial Dashboard</h1>
+        <button className="btn add-account-button">
+          <span>May 2026</span>
+          <span className="icon-[tabler--chevron-down-filled]"></span>
+        </button>
+      </div>
+
+      <div className="cards-row">
+        
+      </div>
+
     </div>
   );
 }
---
+++ BudgetCenter-fe/src/components/BalanceCard/BalanceCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function BalanceCard() {
+    return (
+        <div className="card card-xs sm:max-w-sm m-2">
+            <div className="card-body">
+                <h5 className="card-title mb-2.5">Welcome to Our Service</h5>
+                <p className="mb-4">Discover the features and benefits that our service offers. Enhance your experience with our user-friendly platform designed to meet all your needs.</p>
+            </div>
+        </div>
+    );
+}
