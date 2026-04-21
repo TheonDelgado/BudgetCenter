48f2929245ce49cca5f6979cf8dd8a901d881e4b
TheonDelgado <delgado_theon@yahoo.com>
2026-04-20 22:41:36 -0400

    Balance card styling

--- BudgetCenter-fe/src/components/BalanceCard/BalanceCard.tsx
+++ BudgetCenter-fe/src/components/BalanceCard/BalanceCard.tsx
@@ -5,13 +5,17 @@ export default function BalanceCard() {
     return (
         <div className="card card-xs sm:max-w-sm m-2">
             <div className="card-body">
-                <div className="head">
-                    <h5 className="card-title mb-2.5">Current Balance</h5>
-                    {/* Insert Icon Here */}
+                <div className="mb-2.5 head">
+                    <h5 className="card-title px-2">Current Balance</h5>
+                    <span className="balance-icon icon-[tabler--wallet]"></span>
                 </div>
                 <div className="foot">
-                    <div className="mb-4">$12,000</div>
-                    <div className="mb-4">36%</div>
+                    <div className="balance-number px-2">$12,000.78</div>
+                    <div className="balance-percent ">
+                        <span className="percent-icon icon-[tabler--arrow-up-right]"></span>
+                        <span className="icon-[tabler--plus]"></span>
+                        <span className="percentage">0.36%</span>
+                    </div>
                 </div>
             </div>
         </div>
